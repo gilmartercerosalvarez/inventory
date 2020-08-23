cb4e094fc830a3acc8c0504f86c3dde102447587
gilmarterceros <gilmartercerosalvarez@gmail.com>
2020-08-23 15:10:10 -0400

    Update project name to name set in heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/inventory'));
+app.use(express.static(__dirname + '/dist/mi-inventario'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/inventory/index.html'));});
+    res.sendFile(path.join(__dirname + '/dist/mi-inventario/index.html'));});
 app.listen(process.env.PORT || 8080);
